d25fc0f38f7eb14e82da8d2b5af4eadc43e8ec6c
Fernando Lobo <ferlobo85@gmail.com>
2025-06-26 12:46:36 +0100

    Updated test_merge_fingerprints to meet test data structure

--- test/test_merge_fingerprints.py
+++ test/test_merge_fingerprints.py
@@ -3,19 +3,19 @@ import pandas as pd
 from script.utils import merge_fingerprints
 
 def test_read_csv():
-    file = "data/grover.csv"
+    file = "data/test/grover.csv"
     df = merge_fingerprints.read_csv(file)
     assert isinstance(df, pd.DataFrame)
 
 def test_read_files():
-    files = os.listdir('data')
-    files = [os.path.join('data', file) for file in files]
+    files = os.listdir('data/test')
+    files = [os.path.join('data', 'test', file) for file in files]
     data = merge_fingerprints.read_files(files)
     assert(len(data) == len(files))
 
 def test_merge():
-    files = os.listdir('data')
-    files = [os.path.join('data', file) for file in files]
+    files = os.listdir('data/test')
+    files = [os.path.join('data', 'test', file) for file in files]
     fingerprints = merge_fingerprints.read_files(files)
     df = merge_fingerprints.merge(fingerprints)
     assert df.shape[0] == fingerprints[0].shape[0]
