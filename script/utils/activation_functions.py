89ac68fd9a68bbf1c242237c6669d36d242eb9ce
Fernando Lobo <ferlobo85@gmail.com>
2025-07-02 16:18:03 +0100

    activation_functions module added to utils with get_activation and list_names functions

---
+++ script/utils/activation_functions.py
@@ -0,0 +1,36 @@
+from typing import Callable
+import torch.nn as nn
+
+ACTIVATIONS = {
+        "relu": nn.ReLU,
+        "sigmoid": nn.Sigmoid,
+        "tanh": nn.Tanh,
+        "leaky_relu": nn.LeakyReLU,
+        "gelu": nn.GELU,
+        "elu": nn.ELU,
+    }
+
+def get_activation(name : str) -> Callable:
+    """
+    Retrieves an activation function by name.
+    Args:
+        name (str): The name of the activation function to retrieve.
+    Returns:
+        Callable: The activation function corresponding to the given name.
+    Raises:
+        ValueError: If the specified activation function name is not recognized.
+    """
+    
+    if name not in ACTIVATIONS:
+        raise ValueError(f"Activation '{name}' not recognized")
+    return ACTIVATIONS[name]()
+
+def list_names() -> list[str]:
+    """
+    Returns a list of available activation function names.
+
+    Returns:
+        list[str]: List of activation function names.
+    """
+    return list(ACTIVATIONS.keys())
+    
